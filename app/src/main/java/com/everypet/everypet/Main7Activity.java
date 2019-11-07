62ec2c10d610722a1d3882ddcf271d39482e0821
cars0106 <cars0106@naver.com>
2019-11-07 15:54:24 +0900

    Add All Activities(java files, xml files)
    Signed-off-by: cars0106 <cars0106@naver.com>

---
+++ app/src/main/java/com/everypet/everypet/Main7Activity.java
@@ -0,0 +1,14 @@
+package com.everypet.everypet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main7Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main7);
+    }
+}
