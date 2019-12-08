7a08738872fef17faaaf16c53ee2d569f5dfa8d2
cars0106 <cars0106@naver.com>
2019-12-08 21:00:45 +0900

    Implement Get Image from File
    Signed-off-by: cars0106 <cars0106@naver.com>

--- app/src/main/java/com/everypet/everypet/CommunityAdderWriteActivity.java
+++ app/src/main/java/com/everypet/everypet/CommunityAdderWriteActivity.java
@@ -1,22 +1,60 @@
 package com.everypet.everypet;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.everypet.everypet.font.BaseActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class CommunityAdderWriteActivity extends BaseActivity {
+import java.io.InputStream;
+
+public class CommunityAdderWriteActivity extends BaseActivity implements View.OnClickListener {
+    private final int REQUEST_CODE = 0;
+
+    ImageView imageView;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    InputStream inputStream = getContentResolver().openInputStream(data.getData());
+
+                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                    inputStream.close();
+
+                    imageView.setImageBitmap(bitmap);
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "사진 선택 취소", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +96,11 @@ public class CommunityAdderWriteActivity extends BaseActivity {
                 animalTypeTextView.setText("기타");
                 break;
         }
+
+        Button addImageButton = findViewById(R.id.button_add_image_community_adder_write);
+        addImageButton.setOnClickListener(this);
+
+        imageView = findViewById(R.id.image_view_community_adder);
 
         // BottomNavigationBar implementation
         final BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
@@ -124,5 +167,20 @@ public class CommunityAdderWriteActivity extends BaseActivity {
                 })
                 .setNegativeButton("아니오", null)
                 .show();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.button_add_image_community_adder_write) {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(CommunityAdderWriteActivity.this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+            }
+            else {
+                Intent imageIntent = new Intent();
+                imageIntent.setType("image/*");
+                imageIntent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(imageIntent, REQUEST_CODE);
+            }
+        }
     }
 }
